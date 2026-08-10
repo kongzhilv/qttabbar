f43e12c3244ea49ff5963e937cd1ac1ae04b39f4
kongzhilv <157718507+kongzhilv@users.noreply.github.com>
2026-08-10 12:31:48 +0800

    Keep QTTabBar version bump minimal

--- QTTabBar/Properties/AssemblyInfo.cs
+++ QTTabBar/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Resources;
 [assembly: AssemblyCompany("indiff")]
 [assembly: AssemblyProduct("QTTabBar")]
 // 2007-2020
-[assembly: AssemblyCopyright("Copyright (C)  2007-2026")]
+[assembly: AssemblyCopyright("Copyright (C)  2007-2023")]
 [assembly: AssemblyTrademark("indiff")]
 [assembly: AssemblyCulture("")]
 // [assembly: AssemblyFileVersion("2.0.0.0")]
